fd6ba5efc775a9a37d33a3de78b15a5a460954c0
Michael Moore <michael_m@cimbura.com>
2016-11-29 07:29:36 +0000

    Initial commit

---
+++ wfs.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * WFS provides WFS (Web Feature Service) endpoints for WordPress.
+ *
+ * Plugin Name: WFS
+ * Description: Turn your spatial data into endpoints
+ * Plugin URI: https://github.com/cimburadotcom/wfs
+ * Author: Michael Moore
+ * Author URI: http://cimbura.com
+ * Version: 0.0.1
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wfs
+ * Domain Path: /lang
+ *
+ * @package wfs
+ **/
+
+
